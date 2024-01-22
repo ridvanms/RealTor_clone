e14e56c90f749ef2a33cd25b1bbf4f8b85668e7c
Ridvan <ridvans@abv.bg>
2024-01-22 14:21:13 +0200

    updating

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home"
+import Offers from "./pages/Offers"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import ForgotPassword from "./pages/ForgotPassword"
+import Profile from "./pages/Profile"
 
 function App() {
   return (
     <>
-      <h1 className='text-6xl bg-cyan-200 '>Hello world!</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/offers' element={<Offers/>}/>
+          <Route path='/forgot-password' element={<ForgotPassword/>}/>
+          <Route path='/sign-in' element={<SignIn/>}/>
+          <Route path='/sign-up' element={<SignUp/>}/>
+        </Routes>
+      </Router>
     </>
   );
 }
