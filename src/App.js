cb372153138c0705199a3b615321e6b36860ce87
ridvanms <ridvans@abv.bg>
2024-01-28 13:03:52 +0200

    updating taiwind css

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import ForgotPassword from "./pages/ForgotPassword"
 import Profile from "./pages/Profile"
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <Router>
+        <Header/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/profile' element={<Profile/>}/>
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <header>
+        <div>
+          <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="realTor" className="h-5 cursor-pointer"/>
+        </div>
+      </header>
+    </div>
+  )
+}
