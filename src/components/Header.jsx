0c8f9a69295dc28cafb2121b22171871cad9fd6a
Ridvan <ridvans@abv.bg>
2024-02-04 08:29:01 +0200

    updating

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,16 +19,16 @@ export default function Header() {
         <div>
           <ul className="flex space-x-10">
             <li className={`cursor-pointer py-3 text-sm font-semibold
-             text-gray-400 border-b-[3px] border-b-transparent
+             text-gray-400 border-b-[3px] border-transparent
              ${pathMathRoute("/") && "text-black border-b-red-500"}`}
             onClick={() => navigate('/')}>Home</li>
             <li className={`cursor-pointer py-3 text-sm font-semibold
-             text-gray-400 border-b-[3px] border-b-transparent
+             text-gray-400 border-b-[3px] border-transparent
              ${pathMathRoute("/offers") && "text-black border-b-red-500"}`}
              onClick={()=>navigate('/offers')}>Offers</li>
             <li className={`cursor-pointer py-3 text-sm font-semibold
-             text-gray-400 border-b-[3px] border-b-transparent
-             ${pathMathRoute("/signin") && "text-black border-b-red-500"}`}
+             text-gray-400 border-b-[3px] border-transparent
+             ${pathMathRoute("/sign-in") && "text-black border-b-red-500"}`}
             onClick={()=>navigate('/sign-in')}>Sign in</li>
           </ul>
         </div>
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai"
+import { Link } from 'react-router-dom';
 export default function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
@@ -32,9 +33,9 @@ export default function SignIn() {
               value={email}
               onChange={onChange}
               placeholder="Email address"
-              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
             />
-            <div>
+            <div className="relative mb-6">
               <input
               type={showPassword ? "text" : "password"}
               id="password"
@@ -42,10 +43,21 @@ export default function SignIn() {
               onChange={onChange}
               placeholder="Password"
               className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
-            />
+              autoComplete="off" //trying to remove the second eye in the edge browser
+              />
+              {showPassword ? (<AiFillEyeInvisible className="absolute right-3 top-3 cursor-pointer text-xl" onClick={() => setShowPassword((prevState)=>!prevState)} />)
+                : (<AiFillEye className="absolute right-3 top-3 cursor-pointer text-xl" onClick={() => setShowPassword((prevState) => !prevState)}  />)}
            </div>
-           
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+              <p className="mb-6">Don't have an account ? <Link
+                className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1" to="/sign-up">Register</Link></p>
+              <p >
+                <Link to="/forgot-password"
+                className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out ml-1">Forgot password?</Link>
+              </p>
+            </div>
           </form>
+          <button className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg" type="submit">Sign In</button>
         </div>
       </div>
     </section>
